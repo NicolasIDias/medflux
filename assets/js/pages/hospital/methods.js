3e8f301a537e204e2ab7f3e5be967b046b2bf338
NicolasIDias <nicolasbhgamer@gmail.com>
2025-04-24 21:05:43 -0300

    lol

---
+++ assets/js/pages/hospital/methods.js
@@ -0,0 +1,22 @@
+class Methods {
+
+  async findHospitalById(id){
+
+  }
+  async findHospitalByName(name){
+
+  }
+  async registerHospital(){
+
+  }
+  async listAll(){
+
+  }
+  async listByParam(){
+    
+  }
+
+}
+
+
+export const methods = new Methods()

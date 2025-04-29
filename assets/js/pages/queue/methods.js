30a002036ca2da2a8d204e41ff5c3cd7a72b637e
NicolasIDias <nicolasbhgamer@gmail.com>
2025-04-28 21:51:00 -0300

    saddasd

---
+++ assets/js/pages/queue/methods.js
@@ -0,0 +1,26 @@
+class Methods {
+
+  async listAll(){
+
+  }
+
+  async listAllByHospital(){
+
+  }
+  async findByName(){
+
+  }
+  async findById(){
+
+  }
+
+  async registerQueue(){
+
+  }
+  async registerPatient(){
+    
+  }
+
+}
+
+export const methods = new Methods()
